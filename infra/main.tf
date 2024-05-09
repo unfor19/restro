ce1e7b751a9e17cc7ba6891672e88806e98f81b7
Meir Gabay <unfor19@gmail.com>
2024-05-10 01:00:55 +0300

    added app logs

--- infra/main.tf
+++ infra/main.tf
@@ -33,5 +33,17 @@ resource "azurerm_linux_web_app" "webapp" {
       python_version = 3.9
     }
   }
-}
 
+  logs {
+    application_logs {
+      file_system_level = "Information"
+    }
+
+    http_logs {
+      file_system {
+        retention_in_days = 7
+        retention_in_mb   = 35
+      }
+    }
+  }
+}
